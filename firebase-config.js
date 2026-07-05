1094df7c091edb2107ef39872b28d0927d734fbc
abing717 <abing717@gmail.com>
2026-07-05 18:03:52 +0800

    Add Firebase config

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+window.firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  ownerEmail: "abing717@gmail.com",
+  requireAuth: true,
+  firestoreRootCollection: "electionAdManager",
+  workspaceId: "main"
+};
